c836b76867849b2c1e300b28ce1a0dcdf053f593
jerry <dldydwp76@gmail.com>
2023-11-30 21:23:39 +0900

    일단 + Double 구현

--- CalcCLI/src/calccli/Calculator.kt
+++ CalcCLI/src/calccli/Calculator.kt
@@ -9,6 +9,22 @@ class Calculator {
     var dd = .0
     val operators = "+-*/%"
     fun prnval() = if (tt == 0) println(ll) else println(dd)
+    fun operate(o: Char, b: Double):Double {
+        when (o) {
+            '+' -> return add(b)
+            else -> throw IllegalArgumentException("error: 잘못된 연산자: $o")
+        }
+    }
+    fun operate(o: Char, b: Long) {
+
+    }
+    fun add(b: Double):Double {
+        if (tt == 0) {
+            tt = 1; dd = b + ll
+        }
+        else dd += b
+        return dd
+    }
     fun add(a: Double, b: Double) {
         if (floor(a) == a && floor(b) == b) {
             tt = 0; ll = a.toLong() + b.toLong()
--- CalcCLI/src/calccli/main.kt
+++ CalcCLI/src/calccli/main.kt
@@ -1,7 +1,5 @@
 package calccli
 
-import java.io.StreamTokenizer
-
 fun main() {
     println("계산기 프로그램 입니다. 식을 입력하세요.")
     println("\"숫자 연산자 숫자\" 형태로 입력합니다.")
@@ -13,72 +11,38 @@ fun main() {
     println("==========================")
 
     val calc = Calculator()
-//    var s = ""
-    lateinit var st: StreamTokenizer
-    var tk: Int
+    var s: String
+    lateinit var sl: List<String>
     while (true) {
-        st = StreamTokenizer(System.`in`.bufferedReader())
-        tk = st.nextToken()
-        when (tk) {
-            StreamTokenizer.TT_EOF, StreamTokenizer.TT_EOL -> continue
-            StreamTokenizer.TT_NUMBER -> {
-                val a = st.nval
-                tk = st.nextToken()
-                if (tk != 43) {  // 왠지 모르겠는데 43으로 찍히네...
-                    println("error: 숫자 다음 연산자가 아님. tk = $tk")
-                    continue
-                }
-                val sss = st.sval
-                println("sss: $sss")
-                val o = st.sval[0]
+        s = readln()
+        if (s == "") continue
+        sl = s.split(" ")
+        when {
+            sl[0].toLongOrNull() != null -> {
+
+            }
+
+            sl[0].toDoubleOrNull() != null -> {
+
+            }
+
+            else -> {
+                val o = sl[0][0]
                 if (o !in calc.operators) {
                     println("error: 지원하는 연산자가 아닙니다. o = $o")
                     continue
                 }
-                tk = st.nextToken()
-                if (tk != StreamTokenizer.TT_NUMBER) {
-                    println("error: 문자 다음 숫자가 아님. tk = $tk")
+                if (sl.size < 2) {
+                    println("error: 연산자 뒤에 입력값이 없습니다.)")
                     continue
                 }
-                val b = st.nval
-                when (o) {
-                    '+' -> {
-                        calc.add(a, b)
-                    }
-                    //todo - * / %
+                if (sl[1].toLongOrNull() != null) {
+                    calc.operate(o, sl[1].toLong())
+                } else if (sl[1].toDoubleOrNull() != null) {
+                    calc.operate(o, sl[1].toDouble())
                 }
+
                 calc.prnval()
-            }
-
-            43 -> {  // 왠지 모르겠는데 43으로 찍히네...
-                when (st.sval) {
-                    "q", "Q" -> return
-                    "r", "R" -> {
-                        println("0\n")
-                        continue
-                    }
-
-                    "+" -> {
-                        //Todo +
-                    }
-                    //Todo - * / %
-                    else -> {
-                        println("잘못된 입력입니다.")
-                        continue
-                    }
-                }
-
-                val s = st.sval
-                if (s == "q" || s == "Q") return
-                if (s == "r" || s == "R") {
-                    println("0\n")
-                } else println("잘못된 입력입니다.")
-                continue
-            }
-
-            StreamTokenizer.TT_WORD -> println("error: TT_WORD")
-            else -> {
-                println("error: 입력이 뭔가 잘못됐습니다. token: $tk")
             }
         }
     }
