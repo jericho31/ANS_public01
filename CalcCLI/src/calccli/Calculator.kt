6801963e6a3beabd64ca6f9364a4d255d4fba277
jerry <dldydwp76@gmail.com>
2023-12-01 21:40:29 +0900

    implement - operate 관계 개선, 객체 지향

--- CalcCLI/src/calccli/Calculator.kt
+++ CalcCLI/src/calccli/Calculator.kt
@@ -16,219 +16,77 @@ class Calculator {
     fun implement(s: String): String {
         val sl = s.split(" ")
 
-        if (sl[0].toDoubleOrNull() != null) {
-            if (sl[0].toLongOrNull() != null) {
+        if (sl[0].toDoubleOrNull() != null) {  // 숫자1 + 연산자 + 숫자2
+            if (sl[0].toLongOrNull() != null) {  // 정수1
                 if (sl.size < 2) return "error: 숫자 뒤에 입력값이 없습니다."
                 val o = sl[1][0]
                 if (o !in operators) return "error: 지원하는 연산자가 아닙니다. o = $o"
                 if (sl.size < 3) return "error: 연산자 뒤에 입력값이 없습니다."
                 if (sl[2].toDoubleOrNull() == null) return "error: 두 번째 숫자를 잘못 입력했습니다. ${sl[2]}"
                 val b = sl[2].toLongOrNull()
-                if (b == null) {
-                    operate(sl[0].toDouble(), o, sl[2].toDouble())
-                } else {
-                    operate(sl[0].toLong(), o, b)
+                if (b == null) {  // 소수2
+                    curtype = 1
+                    dd = operate(sl[0].toDouble(), o, sl[2].toDouble())
+                } else {  // 정수2
+                    curtype = 0
+                    ll = operate(sl[0].toLong(), o, b)
                 }
-            } else {
+            } else {  // 소수1
                 if (sl.size < 2) return "error: 숫자 뒤에 입력값이 없습니다."
                 val o = sl[1][0]
                 if (o !in operators) return "error: 지원하는 연산자가 아닙니다. o = $o"
                 if (sl.size < 3) return "error: 연산자 뒤에 입력값이 없습니다."
                 val b = sl[2].toDoubleOrNull() ?: return "error: 두 번째 숫자를 잘못 입력했습니다. ${sl[2]}"
-                operate(sl[0].toDouble(), o, b)
+                curtype = 1
+                dd = operate(sl[0].toDouble(), o, b)
             }
         } else {  // 연산자 + 숫자
             val o = sl[0][0]
             if (o !in operators) return "error: 지원하는 연산자가 아닙니다. o = $o"
             if (sl.size < 2) return "error: 연산자 뒤에 입력값이 없습니다."
             if (sl[1].toDoubleOrNull() == null) return "error: 숫자를 잘못 입력했습니다. ${sl[1]}"
-            val b = sl[1].toLongOrNull()
-            if (b == null) {
-                operate(o, sl[1].toDouble())
+            if (curtype == 0) {
+                val b = sl[1].toLongOrNull()
+                if (b == null) {
+                    curtype = 1
+                    dd = operate(ll.toDouble(), o, sl[1].toDouble())
+                } else {
+                    ll = operate(ll, o, b)
+                }
             } else {
-                operate(o, b)
+                dd = operate(dd, o, sl[1].toDouble())
             }
         }
         return if (curtype == 0) ll.toString() else dd.toString()
     }
 
-    fun operate(o: Char, b: Double): Double =
-        when (o) {
-            '+' -> add(b)
-            '-' -> subtract(b)
-            '*' -> multiply(b)
-            '/' -> divide(b)
-            '%' -> mod(b)
-            else -> throw IllegalArgumentException("error: 잘못된 연산자: $o")
-        }
-
-    fun operate(o: Char, b: Long) {
-        if (curtype == 1) {
-            operate(o, b.toDouble())
-            return
-        }
-        when (o) {
-            '+' -> addLongToLong(b)
-            '-' -> subtractLongToLong(b)
-            '*' -> multiplyLongToLong(b)
-            '/' -> divideLongToLong(b)
-            '%' -> modLongToLong(b)
-            else -> throw IllegalArgumentException("error: 잘못된 연산자: $o")
-        }
-    }
-
+    // implement에서 현재 타입도 확인해서 들어오기 때문에 DoD 혹은 LoL 밖에 없다...가 아니라?
+    // 추가 계산 LoD는 있네;; 그럼 이건 impl쪽에서 현재를 바꿔서 DoD 형태로 불러주면? 될듯!
+    // 추가 계산도 다 aob 형태로 호출하고.
+    // 현재 타입 바꾸고 그런 것도 다.
+    // operate는 "연산만" 하니까 코드 확 정리되네... ㅠㅠ
     fun operate(a: Double, o: Char, b: Double): Double =
         when (o) {
-            '+' -> {
-                curtype = 1; dd = AddOperation.op(a, b); dd
-            }
-
-            '-' -> {
-                curtype = 1; dd = SubtractOperation.op(a, b); dd
-            }
-
-            '*' -> {
-                curtype = 1; dd = MultiplyOperation.op(a, b); dd
-            }
-
-            '/' -> {
-                curtype = 1; dd = DivideOperation.op(a, b); dd
-            }
-
+            '+' -> AddOperation.op(a, b)
+            '-' -> SubtractOperation.op(a, b)
+            '*' -> MultiplyOperation.op(a, b)
+            '/' -> DivideOperation.op(a, b)
             '%' -> mod(a, b)
             else -> throw IllegalArgumentException("error: 잘못된 연산자: $o")
         }
-
-//    fun operate(a: Double, o: Char, b: Long): Double =
-//        when (o) {
-//            '+' -> AddOperation.op(a, b)
-//            '-' -> subtract(a, b)
-//            '*' -> multiply(a, b)
-//            '/' -> divide(a, b)
-//            '%' -> mod(a, b)
-//            else -> throw IllegalArgumentException("error: 잘못된 연산자: $o")
-//        }
-
-//    fun operate(a: Long, o: Char, b: Double): Double =
-//        when (o) {
-//            '+' -> AddOperation.op(a, b)
-//            '-' -> subtract(a, b)
-//            '*' -> multiply(a, b)
-//            '/' -> divide(a, b)
-//            '%' -> mod(a, b)
-//            else -> throw IllegalArgumentException("error: 잘못된 연산자: $o")
-//        }
 
     fun operate(a: Long, o: Char, b: Long): Long =
         when (o) {
-            '+' -> {
-                curtype = 0; ll = AddOperation.op(a, b); ll
-            }
-
-            '-' -> {
-                curtype = 0; ll = SubtractOperation.op(a, b); ll
-            }
-
-            '*' -> {
-                curtype = 0; ll = MultiplyOperation.op(a, b); ll
-            }
-
-            '/' -> {
-                curtype = 0; ll = DivideOperation.op(a, b); ll
-            }
-
+            '+' -> AddOperation.op(a, b)
+            '-' -> SubtractOperation.op(a, b)
+            '*' -> MultiplyOperation.op(a, b)
+            '/' -> DivideOperation.op(a, b)
             '%' -> mod(a, b)
             else -> throw IllegalArgumentException("error: 잘못된 연산자: $o")
         }
 
-    fun add(b: Double): Double {
-        if (curtype == 0) {
-            curtype = 1; dd = AddOperation.op(ll, b)
-        } else dd = AddOperation.op(dd, b)
-        return dd
-    }
-
-    fun addLongToLong(b: Long): Long {
-        if (curtype == 1) {
-            println("warning: 기존값이 $dd Double입니다. 강제로 Long으로 캐스팅합니다.")
-            curtype = 0; ll = AddOperation.op(dd.toLong(), b)
-        } else ll = AddOperation.op(ll, b)
-        return ll
-    }
-
-    fun subtract(b: Double): Double {
-        if (curtype == 0) {
-            curtype = 1; dd = SubtractOperation.op(ll, b)
-        } else dd = SubtractOperation.op(dd, b)
-        return dd
-    }
-
-    fun subtractLongToLong(b: Long): Long {
-        if (curtype == 1) {
-            println("warning: 기존값이 $dd Double입니다. 강제로 Long으로 캐스팅합니다.")
-            curtype = 0; ll = SubtractOperation.op(dd.toLong(), b)
-        } else ll = SubtractOperation.op(ll, b)
-        return ll
-    }
-
-    fun multiply(b: Double): Double {
-        if (curtype == 0) {
-            curtype = 1; dd = MultiplyOperation.op(ll, b)
-        } else dd = MultiplyOperation.op(dd, b)
-        return dd
-    }
-
-    fun multiplyLongToLong(b: Long): Long {
-        if (curtype == 1) {
-            println("warning: 기존값이 $dd Double입니다. 강제로 Long으로 캐스팅합니다.")
-            curtype = 0; ll = MultiplyOperation.op(dd.toLong(), b)
-        } else ll = MultiplyOperation.op(ll, b)
-        return ll
-    }
-
-    fun divide(b: Double): Double {
-        if (curtype == 0) {
-            curtype = 1; dd = DivideOperation.op(ll, b)
-        } else dd = DivideOperation.op(dd, b)
-        return dd
-    }
-
-    fun divideLongToLong(b: Long): Long {
-        if (curtype == 1) {
-            println("warning: 기존값이 $dd Double입니다. 강제로 Long으로 캐스팅합니다.")
-            curtype = 0; ll = DivideOperation.op(dd.toLong(), b)
-        } else ll = DivideOperation.op(ll, b)
-        return ll
-    }
-
-    fun mod(b: Double): Double {
-        if (curtype == 0) {
-            curtype = 1; dd = ll % b
-        } else dd %= b
-        return dd
-    }
-
-    fun modLongToLong(b: Long): Long {
-        if (curtype == 1) {
-            println("warning: 기존값이 $dd Double입니다. 강제로 Long으로 캐스팅합니다.")
-            curtype = 0; ll = dd.toLong() % b
-        } else ll %= b
-        return ll
-    }
-
-    fun mod(a: Double, b: Double): Double {
-        curtype = 1; dd = a / b; return dd
-    }
-
-//    fun mod(a: Double, b: Long): Double {
-//        tt = 1; dd = a / b; return dd
-//    }
-//
-//    fun mod(a: Long, b: Double): Double {
-//        tt = 1; dd = a / b; return dd
-//    }
-
-    fun mod(a: Long, b: Long): Long {
-        curtype = 0; ll = a / b; return ll
-    }
+    fun mod(a: Double, b: Double): Double = a % b
+    fun mod(a: Double, b: Long): Double = a % b
+    fun mod(a: Long, b: Double): Double = a % b
+    fun mod(a: Long, b: Long): Long = a % b
 }
