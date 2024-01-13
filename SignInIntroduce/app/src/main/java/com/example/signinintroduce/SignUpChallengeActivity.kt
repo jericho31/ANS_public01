1078b8310bfe5ff2339fb25fc4bf901ddb51c98e
jericho31 <a@a.a>
2024-01-13 23:37:46 +0900

    뷰바인딩으로 일단 변경

--- SignInIntroduce/app/src/main/java/com/example/signinintroduce/SignUpChallengeActivity.kt
+++ SignInIntroduce/app/src/main/java/com/example/signinintroduce/SignUpChallengeActivity.kt
@@ -10,10 +10,6 @@ import android.view.animation.AlphaAnimation
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Spinner
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
@@ -22,21 +18,13 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.signinintroduce.databinding.ActivitySignUpChallengeBinding
 import java.util.regex.Pattern
 
-// xml로 넣으려했더니 getString 하려면 context가 있어야하네. 그냥 전역으로 냅두는걸로.
-// 입력 길이도 정규식에서 체크할 수 있지만 메세지를 달리 하려면 체크하는 쪽에서도 해야 함.
-//val sample = """^[0-9a-zA-Z!@#$%^+\-=]*$"""  // 참고용
-//val sample = """^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^+\-=])(?=\S+$).*$"""  // 참고용
-//val sample = "^(?=.*[A-Za-z])(?=.*[0-9])[A-Za-z[0-9]]{8,20}$"  // 참고용
-//val sample = "^(?=.*[A-Za-z])(?=.*[$@$!%*#?&.])[A-Za-z$@$!%*#?&.]{8,20}$"  // 참고용
-//val sample = "^(?=.*[0-9])(?=.*[$@$!%*#?&.])[[0-9]$@$!%*#?&.]{8,20}$"  // 참고용
-const val pwInputRegex = """^[0-9a-zA-Z!"#$%&'()*+,-./:;<=>?@\[\]^_`{|}~]*$"""
-const val pwFinalRegex =
-    """^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!"#$%&'()*+,-./:;<=>?@\[\]^_`{|}~])[0-9a-zA-Z0-9a-zA-Z!"#$%&'()*+,-./:;<=>?@\[\]^_`{|}~]{8,16}$"""
-val pwInputPattern: Pattern = Pattern.compile(pwInputRegex)
-val pwFinalPattern: Pattern = Pattern.compile(pwFinalRegex)
-
 class SignUpChallengeActivity : AppCompatActivity() {
-    // 클래스 변수명 정규식인듯: """_?[a-z][A-Za-z\d]*"""
+    val pwInputRegex = """^[0-9a-zA-Z!"#$%&'()*+,-./:;<=>?@\[\]^_`{|}~]*$"""
+    val pwFinalRegex =
+        """^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!"#$%&'()*+,-./:;<=>?@\[\]^_`{|}~])[0-9a-zA-Z0-9a-zA-Z!"#$%&'()*+,-./:;<=>?@\[\]^_`{|}~]{8,16}$"""
+    val pwInputPattern: Pattern = Pattern.compile(pwInputRegex)
+    val pwFinalPattern: Pattern = Pattern.compile(pwFinalRegex)
+    
     private val TAG = "mine"
     private lateinit var binding: ActivitySignUpChallengeBinding
     private lateinit var viewModel: SignUpViewModel
@@ -46,21 +34,8 @@ class SignUpChallengeActivity : AppCompatActivity() {
         val mails = arrayOf("gmail.com", "kakao.com", "naver.com", "직접 입력")
     }
 
-    private val etName: EditText by lazy { findViewById(R.id.et_chall_name) }
-    private val tvNameWarn: TextView by lazy { findViewById(R.id.tv_chall_name_warn) }
-    private val etMail: EditText by lazy { findViewById(R.id.et_chall_mail) }
-    private val etDomain: EditText by lazy { findViewById(R.id.et_chall_domain) }
-    private val tvMailWarn: TextView by lazy { findViewById(R.id.tv_chall_mail_warn) }
-    private val etPw: EditText by lazy { findViewById(R.id.et_chall_password) }
-    private val tvPwWarn: TextView by lazy { findViewById(R.id.tv_chall_password_warn) }
-    private val tvPwLength: TextView by lazy { findViewById(R.id.tv_chall_password_length) }
-    private val etVerify: EditText by lazy { findViewById(R.id.et_chall_verify) }
-    private val tvVerifyWarn: TextView by lazy { findViewById(R.id.tv_chall_verify_warn) }
-    private val btn: Button by lazy { findViewById(R.id.btn_chall_signup) }
-    private val spinner: Spinner by lazy { findViewById(R.id.spnr_chall_mail) }
-
     // EditText 배열
-    private val arrET by lazy { arrayOf(etName, etMail, etDomain, etPw, etVerify) }
+    private val arrET by lazy { arrayOf(binding.etChallName, binding.etChallMail, binding.etChallDomain, binding.etChallPassword, binding.etChallVerify) }
 
     private var okName = false
     private var okMail = false
@@ -81,8 +56,8 @@ class SignUpChallengeActivity : AppCompatActivity() {
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 Log.d(TAG, "before:: start: $start, count: $count, after: $after")
-                Log.d(TAG, "$s/selection: ${etPw.selectionStart}, ${etPw.selectionEnd}")
-                beforePw = etPw.text.toString()
+                Log.d(TAG, "$s/selection: ${binding.etChallPassword.selectionStart}, ${binding.etChallPassword.selectionEnd}")
+                beforePw = binding.etChallPassword.text.toString()
                 beforeCursor = start
             }
 
@@ -93,7 +68,7 @@ class SignUpChallengeActivity : AppCompatActivity() {
                 )
                 Log.d(
                     TAG,
-                    "$s/selection: ${etPw.selectionStart}, ${etPw.selectionEnd}"
+                    "$s/selection: ${binding.etChallPassword.selectionStart}, ${binding.etChallPassword.selectionEnd}"
                 )
             }
 
@@ -101,21 +76,21 @@ class SignUpChallengeActivity : AppCompatActivity() {
             // 한글은 조합때문에 입력받고 무시하기 어려울텐데, inputType textPassword 가 알아서 막아주네.
             // 원표시₩는 그냥 입력이 안됨.
             override fun afterTextChanged(s: Editable?) {
-                Log.d(TAG, "after:: $s/selection: ${etPw.selectionStart}, ${etPw.selectionEnd}")
-                if (!pwInputPattern.matcher(etPw.text.toString()).matches()) {
+                Log.d(TAG, "after:: $s/selection: ${binding.etChallPassword.selectionStart}, ${binding.etChallPassword.selectionEnd}")
+                if (!pwInputPattern.matcher(binding.etChallPassword.text.toString()).matches()) {
                     // warn 바꾸는 쪽으로. 그냥 바꾸면 잘 안보일듯. 깜빡여야. 그러려면 코루틴 블라킹으로...?
                     // AlphaAnimation 이라는 좋은 기능이 있었다. 역시 구글갓
-                    tvPwWarn.text = getString(R.string.chall_warn_forbidden_input)
-                    tvPwWarn.startAnimation(anim)
+                    binding.tvChallPasswordWarn.text = getString(R.string.chall_warn_forbidden_input)
+                    binding.tvChallPasswordWarn.startAnimation(anim)
                     // TODO: InputFilter - 일단 구현했으니 다음 기회에.
-                    etPw.removeTextChangedListener(this)
-                    etPw.setText(beforePw)
-                    etPw.setSelection(beforeCursor)
-                    etPw.addTextChangedListener(this)
+                    binding.etChallPassword.removeTextChangedListener(this)
+                    binding.etChallPassword.setText(beforePw)
+                    binding.etChallPassword.setSelection(beforeCursor)
+                    binding.etChallPassword.addTextChangedListener(this)
                     return
                 }
-                "${etPw.text.length}/16".also { tvPwLength.text = it }
-                check(etPw)
+                "${binding.etChallPassword.text.length}/16".also { binding.tvChallPasswordLength.text = it }
+                check(binding.etChallPassword)
                 Log.d(TAG, "== back from check ==")
             }
         }
@@ -128,39 +103,37 @@ class SignUpChallengeActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this)[SignUpViewModel::class.java]
         initViewModel(viewModel)
 
-        // TODO: onCreate에 만든 변수가 어떻게 유지될 수 있는거지...?
-
         // 처음에 기본으로 첫번째꺼가 선택되어 있는 이유는 어레이어댑터라서 그런가?
-        spinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, mails)
+        binding.spnrChallMail.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, mails)
 
-        arrET.forEach { et -> if ((et == etPw).not()) et.addTextChangedListener { check(et) } }
-        etPw.addTextChangedListener(pwWatcher)
+        arrET.forEach { et -> if ((et == binding.etChallPassword).not()) et.addTextChangedListener { check(et) } }
+        binding.etChallPassword.addTextChangedListener(pwWatcher)
         val onFocusChangeListener =
             OnFocusChangeListener { v, hasFocus -> if (!hasFocus) check(v!!) }
         arrET.forEach { it.onFocusChangeListener = onFocusChangeListener }
 
-        btn.setOnClickListener {
-            if (spinner.isVisible) {
-                intent.putExtra(Extra.id, "${etMail.text}@${spinner.selectedItem}")
+        binding.btnChallSignup.setOnClickListener {
+            if (binding.spnrChallMail.isVisible) {
+                intent.putExtra(Extra.id, "${binding.etChallMail.text}@${binding.spnrChallMail.selectedItem}")
             } else {
-                intent.putExtra(Extra.id, "${etMail.text}@${etDomain.text}")
+                intent.putExtra(Extra.id, "${binding.etChallMail.text}@${binding.etChallDomain.text}")
             }
-            intent.putExtra(Extra.password, etPw.text.toString())
+            intent.putExtra(Extra.password, binding.etChallPassword.text.toString())
             setResult(RESULT_OK, intent)
             finish()
         }
 
-        spinner.onItemSelectedListener =
+        binding.spnrChallMail.onItemSelectedListener =
             object : OnItemSelectedListener {
                 override fun onItemSelected(
                     parent: AdapterView<*>?, view: View?, position: Int, id: Long
                 ) {
-                    if (position == spinner.adapter.count - 1) {
-                        etDomain.isVisible = true
-                        spinner.isVisible = false
+                    if (position == binding.spnrChallMail.adapter.count - 1) {
+                        binding.etChallDomain.isVisible = true
+                        binding.spnrChallMail.isVisible = false
                     }
 
-                    check(spinner)
+                    check(binding.spnrChallMail)
                 }
 
                 override fun onNothingSelected(parent: AdapterView<*>?) {}
@@ -177,67 +150,67 @@ class SignUpChallengeActivity : AppCompatActivity() {
             }, check count: ${++checkCount}"
         )
         when (v) {
-            etName -> {
-                if (etName.text.isEmpty()) {
-                    tvNameWarn.text = getString(R.string.chall_warn_enter_name)
+            binding.etChallName -> {
+                if (binding.etChallName.text.isEmpty()) {
+                    binding.tvChallNameWarn.text = getString(R.string.chall_warn_enter_name)
                     okName = false
                 } else {
-                    tvNameWarn.text = ""
+                    binding.tvChallNameWarn.text = ""
                     okName = true
                 }
             }
 
-            etMail, etDomain -> {
-                okMail = etMail.text.isNotEmpty()
-                okDomain = etDomain.text.isNotEmpty()
+            binding.etChallMail, binding.etChallDomain -> {
+                okMail = binding.etChallMail.text.isNotEmpty()
+                okDomain = binding.etChallDomain.text.isNotEmpty()
 
-                if (spinner.isVisible) {
-                    if (!okMail) tvMailWarn.text = getString(R.string.signup_warn_enter_email)
-                    else tvMailWarn.text = ""
+                if (binding.spnrChallMail.isVisible) {
+                    if (!okMail) binding.tvChallMailWarn.text = getString(R.string.signup_warn_enter_email)
+                    else binding.tvChallMailWarn.text = ""
                 } else {
-                    if (!okMail) tvMailWarn.text = getString(
+                    if (!okMail) binding.tvChallMailWarn.text = getString(
                         R.string.signup_warn_enter_email_format,
                         if (!okDomain) getString(R.string.signup_warn_and_domain) else ""
                     )
-                    else if (!okDomain) tvMailWarn.text =
+                    else if (!okDomain) binding.tvChallMailWarn.text =
                         getString(R.string.signup_warn_enter_domain)
-                    else tvMailWarn.text = ""
+                    else binding.tvChallMailWarn.text = ""
                 }
             }
 
-            etPw -> {
-                val pw = etPw.text.toString()
-                if (etPw.text.isEmpty()) {
-                    tvPwWarn.text = getString(R.string.chall_warn_enter_password)
+            binding.etChallPassword -> {
+                val pw = binding.etChallPassword.text.toString()
+                if (binding.etChallPassword.text.isEmpty()) {
+                    binding.tvChallPasswordWarn.text = getString(R.string.chall_warn_enter_password)
                     okPw = false
                 } else if (pw.length < 8) {
-                    tvPwWarn.text = getString(R.string.chall_warn_pw_under_8)
+                    binding.tvChallPasswordWarn.text = getString(R.string.chall_warn_pw_under_8)
                     okPw = false
                 } else if (pw.length > 16) {
-                    tvPwWarn.text = getString(R.string.chall_warn_pw_over_16)
+                    binding.tvChallPasswordWarn.text = getString(R.string.chall_warn_pw_over_16)
                     okPw = false
                 } else if (!pwFinalPattern.matcher(pw).matches()) {
-                    tvPwWarn.text = getString(R.string.chall_warn_pw_regex)
+                    binding.tvChallPasswordWarn.text = getString(R.string.chall_warn_pw_regex)
                     okPw = false
                     // TODO: 동일한 문자를 많이 포함한 경우, 이름이나 메일 등 개인정보.
                 } else {
-                    tvPwWarn.text = ""
+                    binding.tvChallPasswordWarn.text = ""
                     okPw = true
                 }
             }
 
-            etVerify -> {
-                if (etVerify.text.contentEquals(etPw.text)) {
-                    tvVerifyWarn.text = ""
+            binding.etChallVerify -> {
+                if (binding.etChallVerify.text.contentEquals(binding.etChallPassword.text)) {
+                    binding.tvChallVerifyWarn.text = ""
                     okVerify = true
                 } else {
-                    tvVerifyWarn.text = getString(R.string.chall_warn_verify_not_same)
+                    binding.tvChallVerifyWarn.text = getString(R.string.chall_warn_verify_not_same)
                     okVerify = false
                 }
             }
         }
 
-        btn.isEnabled = okName && okMail && (spinner.isVisible || okDomain) && okPw && okVerify
+        binding.btnChallSignup.isEnabled = okName && okMail && (binding.spnrChallMail.isVisible || okDomain) && okPw && okVerify
     }
 
     private fun toastShort(s: String) = Toast.makeText(this, s, Toast.LENGTH_SHORT).show()
--- SignInIntroduce/app/src/main/java/com/example/signinintroduce/SignUpViewModel.kt
+++ SignInIntroduce/app/src/main/java/com/example/signinintroduce/SignUpViewModel.kt
@@ -3,8 +3,15 @@ package com.example.signinintroduce
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.util.regex.Pattern
 
 class SignUpViewModel : ViewModel() {
+    val pwInputRegex = """^[0-9a-zA-Z!"#$%&'()*+,-./:;<=>?@\[\]^_`{|}~]*$"""
+    val pwFinalRegex =
+        """^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!"#$%&'()*+,-./:;<=>?@\[\]^_`{|}~])[0-9a-zA-Z0-9a-zA-Z!"#$%&'()*+,-./:;<=>?@\[\]^_`{|}~]{8,16}$"""
+    val pwInputPattern: Pattern = Pattern.compile(pwInputRegex)
+    val pwFinalPattern: Pattern = Pattern.compile(pwFinalRegex)
+
     private val _text1 = MutableLiveData<String>()
     val text1: LiveData<String> get() = _text1
     fun setText1(text: String) {
