5748b649719bce8b276b0328d538f6c930af4ffd
jericho31 <a@a.a>
2024-02-04 18:37:20 +0900

    Chall_ListAdapter diffUtil 바로 집어넣음

--- Chall_ListAdapter/app/src/main/java/com/example/chall_listadapter/assignment/todo/list/TodoListAdapter.kt
+++ Chall_ListAdapter/app/src/main/java/com/example/chall_listadapter/assignment/todo/list/TodoListAdapter.kt
@@ -13,33 +13,26 @@ import com.example.chall_listadapter.databinding.TodoListItemBinding
 class TodoListAdapter(
     private val onItemClick: (view: View, position: Int) -> Unit,
     private val onSwitchClick: (model: TodoModel) -> Unit
-) : ListAdapter<TodoModel, TodoListAdapter.TodoListViewHolder>(diffUtil) {
+) : ListAdapter<TodoModel, TodoListAdapter.TodoListViewHolder>(object :
+    DiffUtil.ItemCallback<TodoModel>() {
+    override fun areItemsTheSame(oldItem: TodoModel, newItem: TodoModel): Boolean =
+        oldItem.id == newItem.id
+
+    override fun areContentsTheSame(oldItem: TodoModel, newItem: TodoModel): Boolean =
+        oldItem == newItem
+}) {
     inner class TodoListViewHolder(private val binding: TodoListItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun onBind(model: TodoModel) = binding.also { b ->
             b.title.text = model.title
             b.description.text = model.content
             b.root.setOnClickListener {
-                Log.d("myTag", "$layoutPosition: ${getItem(layoutPosition).toString()}")
+                Log.d("myTag", "$layoutPosition: ${getItem(layoutPosition)}")
                 onItemClick(it, layoutPosition)
             }
             b.switchBookmark.setOnClickListener {
                 onSwitchClick(model)
             }
-        }
-    }
-
-    companion object {
-        val diffUtil = object : DiffUtil.ItemCallback<TodoModel>() {
-            override fun areItemsTheSame(oldItem: TodoModel, newItem: TodoModel): Boolean =
-                if (oldItem is TodoModel && newItem is TodoModel) {
-                    oldItem.id == newItem.id
-                } else {
-                    oldItem == newItem
-                }
-
-            override fun areContentsTheSame(oldItem: TodoModel, newItem: TodoModel): Boolean =
-                oldItem == newItem
         }
     }
 
